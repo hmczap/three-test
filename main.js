608dbc1a24b853ec3543220f903783ff700706ff
hmczap <114511170+hmczap@users.noreply.github.com>
2024-02-08 17:31:27 -0500

    Import threeJS to generate canvas, scene, camera and render a basic wireframe cube

--- main.js
+++ main.js
@@ -2,6 +2,46 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+import * as THREE from 'three';
+
+const threejscanvas = document.querySelector('#threejs-canvas');
+let width = threejscanvas.offsetWidth;
+let height= threejscanvas.offsetHeight;
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, 
+  window.innerWidth/window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(1,1,1);
+const material = new THREE.MeshBasicMaterial({color: 0xFF0000, wireframe: true});
+const cube = new THREE.Mesh(geometry, material);
+
+scene.add(cube);
+camera.position.z = 5;
+
+function animate(){
+  requestAnimationFrame( animate );
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.02;
+
+	renderer.render( scene, camera );
+}
+animate();
+
+
+/*
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const controls = new OrbitControls( camera, renderer.domElement );
+const loader = new GLTFLoader();
+*/
+
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -11,7 +51,7 @@ document.querySelector('#app').innerHTML = `
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
+    <h1>Built with Vite</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
